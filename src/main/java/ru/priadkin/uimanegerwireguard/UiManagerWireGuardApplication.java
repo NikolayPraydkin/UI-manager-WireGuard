f6ee44496a81c3111ab4d697004c3aaba0934a8d
NikolayPraydkin <mail2193@yandex.ru>
2022-07-17 10:43:41 +0300

    first commit

---
+++ src/main/java/ru/priadkin/uimanegerwireguard/UiManagerWireGuardApplication.java
@@ -0,0 +1,13 @@
+package ru.priadkin.uimanegerwireguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiManagerWireGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiManagerWireGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/priadkin/uimanegerwireguard/UiManagerWireGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.priadkin.uimanegerwireguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiManagerWireGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
