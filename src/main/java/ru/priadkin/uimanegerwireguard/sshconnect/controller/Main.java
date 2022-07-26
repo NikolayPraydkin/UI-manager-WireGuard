1a2ef13b27a9ce4f17f4dfa8436adef6da3f17c1
NikolayPraydkin <mail2193@yandex.ru>
2022-07-26 18:24:48 +0300

    add rest method removewireguard

--- src/main/java/ru/priadkin/uimanegerwireguard/sshconnect/controller/Main.java
+++ src/main/java/ru/priadkin/uimanegerwireguard/sshconnect/controller/Main.java
@@ -4,13 +4,24 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.priadkin.uimanegerwireguard.sshconnect.domain.SSH;
 import ru.priadkin.uimanegerwireguard.sshconnect.domain.Status;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/")
@@ -158,6 +169,65 @@ public class Main {
                 try {
                     channel = (ChannelExec) v.openChannel("exec");
                     channel.setCommand("echo poker | sudo -S apt install wireguard");
+                    InputStream in = channel.getInputStream();
+                    InputStream errStream = channel.getErrStream();
+
+                    channel.connect();
+                    StringBuilder builder = new StringBuilder();
+                    while (true) {
+                        while (in.available() > 0) {
+                            byte[] tmp = new byte[in.available()];
+                            int i = in.read(tmp, 0, in.available());
+                            if (i < 0) break;
+                            builder.append(new String(tmp, 0, i));
+                        }
+                        while (errStream.available() > 0) {
+                            byte[] tmper = new byte[errStream.available()];
+                            int i = errStream.read(tmper, 0, errStream.available());
+                            if (i < 0) break;
+                            builder.append(new String(tmper, 0, i));
+                        }
+                        if (channel.isClosed()) {
+                            if (in.available() > 0) continue;
+                            System.out.println("exit-status: " + channel.getExitStatus());
+                            break;
+                        }
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception ee) {
+                        }
+                    }
+                    String statusWG = builder.toString();
+                    if (statusWG.contains("Уже установлен пакет wireguard") || statusWG.contains("Настраивается пакет wireguard ")) {
+                        status.setInstaledWireguard(true);
+                    }
+                    status.setMessage(statusWG);
+
+                    channel.disconnect();
+
+                } catch (JSchException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        if (sessions.size() == 0) {
+            status.setMessage("No one session is connected");
+        }
+        return status;
+    }
+
+    @GetMapping("/removewireguard")
+    public Status removewg(@RequestParam(name = "host") String host) {
+        Status status = new Status();
+        sessions.forEach((k, v) -> {
+            ChannelExec channel = null;
+            if (v.getHost().equals(host)) {
+                try {
+                    channel = (ChannelExec) v.openChannel("exec");
+                    channel.setCommand("echo poker | sudo -S apt --purge remove -y wireguard");
+
                     InputStream in = channel.getInputStream();
                     InputStream errStream = channel.getErrStream();
 
