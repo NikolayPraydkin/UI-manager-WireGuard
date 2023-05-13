916f4f975745038aa62b727814c06e486e0255f4
NikolayPraydkin <mail2193@yandex.ru>
2023-05-13 10:10:08 +0300

    draft connection

--- src/angular-frontend/src/app/app.module.ts
+++ src/angular-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import {FormsModule} from "@angular/forms";
   ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        HttpClientModule
     ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ConnectionComponent]
 })
 export class AppModule { }
--- src/angular-frontend/src/app/connection/connection.component.ts
+++ src/angular-frontend/src/app/connection/connection.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, throwError} from "rxjs";
 
 @Component({
   selector: 'app-connection',
@@ -7,16 +9,45 @@ import {Component} from '@angular/core';
 })
 export class ConnectionComponent {
 
-  login: String = ''
-  password: String = ''
+  host: string = ''
+  port?: number
+  login: string = ''
+  password: string = ''
+  result: any = ''
 
-  getLogin(){
+  constructor(private http: HttpClient) {
+  }
+
+  getLogin() {
     return this.login;
   }
 
-  connect() {
-    if(this.login !== '' && this.password !== ''){
-      console.log('connect')
+  connect(): void {
+    if (this.login !== '' && this.password !== '') {
+      let httpParams = new HttpParams().appendAll({'host': this.host, 'user': this.login, 'password': this.password});
+      if (this.port !== undefined) {
+        httpParams.append('port', this.port)
+      }
+      this.http.get('http://localhost:8080/connectSSHByPassword',
+        {responseType: 'text', observe: 'response', params: httpParams})
+        .pipe(catchError((e) => this.handleError(e)))
+        .subscribe(data => this.result = data.body);
     }
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+
+      this.result = error.error
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
